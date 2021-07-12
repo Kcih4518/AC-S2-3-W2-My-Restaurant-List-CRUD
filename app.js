0cbf1edde57b4c3fb114fa67633c38ca1084b115
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-12 12:55:42 +0800

    <feat>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Set up mongoose connect
    1. Set up connection
    2. Get connection status: set db
    3. Handling DeprecationWarning warning
    4. DB: localhost/restaurant-list

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 // Require node_modules
 const express = require('express')
+const mongoose = require('mongoose')
 
 // Define server info
 const port = 3000
 
 // Setting express
 const app = express()
+
+// Setting mongoose
+// TODO: Do more db error handling (like retry )
+/* ------------------------------------------------------ */
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+/// Get database connection status
+const db = mongoose.connection
+
+/// Connection exception handling
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+/// Connection successfully handling
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+/* ------------------------------------------------------ */
 
 // Handle request and response
 app.get('/', (req, res) => {
