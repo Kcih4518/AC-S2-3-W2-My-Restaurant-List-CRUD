db4acddf35ac7b474ca4f08e05f11086f3dda75b
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-12 13:38:29 +0800

    <feat>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Define restaurant Schema
    1. $ mkdir models
    2. $ touch models/restaurant-list.js
    3. Create schema ref restaurant.json
    4. Export module restaurantSchema as Restaurant

---
+++ models/restaurant-list.js
@@ -0,0 +1,46 @@
+// Require node_modules
+const mongoose = require('mongoose')
+
+// Define restaurant-list schema
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  name_en: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  google_map: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+
+// Export module
+module.exports = mongoose.model('Restaurant', restaurantSchema)
