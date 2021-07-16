fe12eda869fdd3c1c350037fdfbf9af6a79ef237
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-16 10:44:02 +0800

    <refactor>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Package mongoose into modules

--- app.js
+++ app.js
@@ -2,38 +2,17 @@
 // TODO: Make sure all route paths are correct
 // Require node_modules
 const express = require('express')
-const mongoose = require('mongoose')
 const exphdbs = require('express-handlebars')
 const turnOnAddButton = require('./helpers/handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
 // Define server info
 const port = 3000
 
 // Setting express
 const app = express()
-
-// Setting mongoose
-// TODO: Do more db error handling (like retry )
-// TODO: Package the mongoose connection into a module
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-// 1. Get database connection status
-const db = mongoose.connection
-
-// 2. Connection exception handling
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 3. Connection successfully handling
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // Setting express-handlebars
 app.engine(
---
+++ config/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+// Setting mongoose
+// TODO: Do more db error handling (like retry )
+// TODO: Package the mongoose connection into a module
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// 1. Get database connection status
+const db = mongoose.connection
+
+// 2. Connection exception handling
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 3. Connection successfully handling
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
