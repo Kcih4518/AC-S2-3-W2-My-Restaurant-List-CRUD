5c874e11327776f686459aa0163638be10319c9c
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-17 23:05:23 +0800

    <refactor>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Modify restaurantSeed.js to more streamlined
    1. When data is written, it will be closed directly

--- models/seeds/restaurantSeed.js
+++ models/seeds/restaurantSeed.js
@@ -9,19 +9,8 @@ const db = require('../../config/mongoose')
 // TODO: Can also use map + join experiment instead
 // FIXME: Maybe also need import restaurant.id into database
 db.once('open', () => {
-  const restaurantListData = restaurantListJson.results
-  restaurantListData.forEach((restaurant) => {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description,
-    })
+  Restaurant.create(restaurantListJson.results).then(() => {
+    db.close()
+    console.log('Import restaurant data into database successfully!')
   })
-  console.log('Import restaurant data into database successfully!')
 })
