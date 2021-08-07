ded8023caf35401f5b02dc2dfc8ca066e973b223
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-07 21:21:39 +0800

    <feat>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Add login prompt

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next()
     }
+    req.flash('warning_msg', 'Please log in before you can use it!')
     res.redirect('/users/login')
   }
 }
