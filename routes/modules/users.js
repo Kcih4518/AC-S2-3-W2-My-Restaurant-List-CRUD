80f090508440e06bfbf78ef9368fa3fd5d42716e
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-07 17:13:53 +0800

    <style>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Modify users.js 's code comments

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
 
-// Read : Display login page
+// READ : Display login page
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// READ : Display register page
 router.get('/register', (req, res) => {
   res.render('register')
 })
