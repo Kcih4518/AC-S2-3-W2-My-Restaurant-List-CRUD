8054debec047c1d94fb153d5252c5596027d062e
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-08 00:16:08 +0800

    <feat>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Modify restaurantSeed.js to create two users and their restaurants

--- models/seeds/restaurantSeed.js
+++ models/seeds/restaurantSeed.js
@@ -1,16 +1,54 @@
 // Require node_modules
-const restaurantListJson = require('./restaurant.json')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const restaurantListJson = require('./restaurant.json').results
 const Restaurant = require('../restaurant-list')
-
+const User = require('../user')
 const db = require('../../config/mongoose')
+
+const SEED_USERS = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantList: restaurantListJson.slice(0, 3)
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantList: restaurantListJson.slice(4, 7)
+  }
+]
 
 // Setting mongoose
 /// Connection successfully handling
-// TODO: Can also use map + join experiment instead
-// FIXME: Maybe also need import restaurant.id into database
 db.once('open', () => {
-  Restaurant.create(restaurantListJson.results).then(() => {
-    db.close()
-    console.log('Import restaurant data into database successfully!')
-  })
+  Promise.all(
+    Array.from(SEED_USERS, (SEED_USER, i) => {
+      const restaurantList = SEED_USER.restaurantList
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+        .then((hash) =>
+          User.create({
+            name: SEED_USER.name,
+            email: SEED_USER.email,
+            password: hash
+          })
+        )
+        .then((user) => {
+          const userId = user._id
+          restaurantList.forEach((restaurant) => (restaurant.userId = userId))
+          return Restaurant.create(restaurantList)
+        })
+    })
+  )
+    .then(() => {
+      console.log('Import restaurant data into database successfully!')
+      process.exit()
+    })
+    .catch((err) => console.log(err))
 })
