4706233c260132a8f5ea1f9e3999587766130fdb
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-09 20:00:24 +0800

    <refactor>[AC-S2-3-W2-My-Restaurant-List-CRUD]: Modify model name restaurant-list.js to restaurant.js

---
+++ models/restaurant.js
@@ -0,0 +1,50 @@
+// Require node_modules
+const mongoose = require('mongoose')
+
+// Define restaurant schema
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+// Export module
+module.exports = mongoose.model('Restaurant', restaurantSchema)
